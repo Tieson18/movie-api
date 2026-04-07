caa45bcc22ee3fc3547faf97cc708c8cea8e112e
Tieson18 <tieson360@gmail.com>
2026-04-06 23:13:03 -0500

    Implement kiota client

--- client/movieClient.ts
+++ client/movieClient.ts
@@ -86,7 +86,7 @@ export function createMovieClient(requestAdapter: RequestAdapter) {
     requestAdapter.baseUrl === ""
   ) {
     requestAdapter.baseUrl =
-      "https://movie-catalog-api-fwehcpdmh6bggtak.canadacentral-01.azurewebsites.net/movies";
+      "https://movie-catalog-api-fwehcpdmh6bggtak.canadacentral-01.azurewebsites.net/";
   }
   const pathParameters: Record<string, unknown> = {
     baseurl: requestAdapter.baseUrl,
