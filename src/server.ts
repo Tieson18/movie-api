28d15de8f20a4a2aca837f3d10c062c6a774e1e7
Tieson18 <tieson360@gmail.com>
2026-04-08 19:46:56 -0500

    fixing azure app service

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import app from "./app.ts";
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`✓ Server running on http://localhost:${port}`);
   console.log(`✓ API Docs available at http://localhost:${port}/docs`);
